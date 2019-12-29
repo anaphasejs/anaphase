07ba09d7d41b5686bc716a644c46db2f19d798fb
Ian Macalinao <me@ian.pw>
2019-12-29 12:27:32 -0600

    don't make the app class

--- packages/core/src/initAnaphase.tsx
+++ packages/core/src/initAnaphase.tsx
@@ -3,8 +3,8 @@ import * as Sentry from "@sentry/browser";
 import { NormalizedCacheObject } from "apollo-cache-inmemory";
 import ApolloClient from "apollo-client";
 import { ApolloAppContext } from "next-with-apollo";
-import App, { AppContext, AppInitialProps } from "next/app";
-import React, { createContext, useContext } from "react";
+import { AppContext, AppInitialProps } from "next/app";
+import React, { createContext, PropsWithChildren, useContext } from "react";
 import ErrorBoundary from "./ErrorBoundary";
 import NProgressContainer from "./NProgressContainer";
 import { withApollo } from "./withApollo";
@@ -21,8 +21,15 @@ interface Args<U> {
   }) => Promise<U | null>;
 }
 
+type AppWrapperProps<U> = PropsWithChildren<{
+  currentUser: U | null;
+  apollo: ApolloClient<NormalizedCacheObject>;
+}>;
+
 interface Anaphase<U> {
-  App: React.Component;
+  AppWrapper: React.FC<AppWrapperProps<U>>;
+  getInitialProps: (ctx: AppContext) => Promise<Omit<Props<U>, "apollo">>;
+  withApollo: any;
   useUser: () => U | null;
 }
 
@@ -36,42 +43,43 @@ export default function initAnaphase<U>({
 
   const UserContext = createContext<U | null>(null);
 
-  class AnaphaseApp extends App<Props<U>> {
-    static async getInitialProps({
-      Component,
-      ctx
-    }: AppContext): Promise<Omit<Props<U>, "apollo">> {
-      let pageProps = {};
-      if (Component.getInitialProps) {
-        pageProps = await Component.getInitialProps(ctx);
-      }
-      return {
-        pageProps,
-        currentUser: await fetchUser({
-          apollo: (ctx as ApolloAppContext<NormalizedCacheObject>).apolloClient
-        })
-      };
+  const getInitialProps = async ({
+    Component,
+    ctx
+  }: AppContext): Promise<Omit<Props<U>, "apollo">> => {
+    let pageProps = {};
+    if (Component.getInitialProps) {
+      pageProps = await Component.getInitialProps(ctx);
     }
+    return {
+      pageProps,
+      currentUser: await fetchUser({
+        apollo: (ctx as ApolloAppContext<NormalizedCacheObject>).apolloClient
+      })
+    };
+  };
 
-    render(): JSX.Element {
-      const { Component, pageProps, apollo, currentUser } = this.props;
-      return (
-        <ErrorBoundary fullPage={true}>
-          <ApolloProvider client={apollo}>
-            <UserContext.Provider value={currentUser}>
-              <Component {...pageProps} />
-            </UserContext.Provider>
-          </ApolloProvider>
-          <NProgressContainer />
-        </ErrorBoundary>
-      );
-    }
-  }
+  const AppWrapper: React.FC<AppWrapperProps<U>> = ({
+    currentUser,
+    apollo,
+    children
+  }: AppWrapperProps<U>) => {
+    return (
+      <ErrorBoundary fullPage={true}>
+        <ApolloProvider client={apollo}>
+          <UserContext.Provider value={currentUser}>
+            {children}
+          </UserContext.Provider>
+        </ApolloProvider>
+        <NProgressContainer />
+      </ErrorBoundary>
+    );
+  };
 
   return {
-    // eslint-disable-next-line @typescript-eslint/ban-ts-ignore
-    // @ts-ignore
-    App: withApollo(AnaphaseApp),
+    AppWrapper,
+    getInitialProps,
+    withApollo,
     useUser: (): U | null => useContext(UserContext)
   };
 }
